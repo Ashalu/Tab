aca1516e4122b14d9d2f533ec71d1d57656fd31e
Developer.Aashish <akbrother02@gmail.com>
2017-06-28 12:28:21 +0530

    success

---
+++ app/src/main/java/com/developer/aashish/drag/tab1.java
@@ -0,0 +1,22 @@
+package com.developer.aashish.drag;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+/**
+ * Created by Aashish on 6/28/2017.
+ */
+
+public class tab1 extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab1,container,false);
+    }
+}
---
+++ app/src/main/java/com/developer/aashish/drag/tab3.java
@@ -0,0 +1,18 @@
+package com.developer.aashish.drag;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Aashish on 6/28/2017.
+ */
+
+public class tab3 extends android.support.v4.app.Fragment {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab3, container, false);
+    }
+}
